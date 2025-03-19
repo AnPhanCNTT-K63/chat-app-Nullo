7ff6013f87208c72fe86126c09f187a3cadce918
AnPhan <126398348+AnPhanCNTT-K63@users.noreply.github.com>
2025-03-19 17:15:18 +0700

    fix: .env

--- frontend-flutter/lib/main.dart
+++ frontend-flutter/lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:app_chat_nullo/providers/user_provider.dart';
 import 'package:app_chat_nullo/routes/router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  print("API_URL: ${dotenv.env['API_URL']}");
+
   AppRouter.setupRouter();
+
   runApp(
     MultiProvider(
       providers: [
